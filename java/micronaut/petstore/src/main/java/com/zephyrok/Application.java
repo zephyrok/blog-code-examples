1f085bad82920f74d05ccef2bf7439b8475f1012
Rafael Huerta <zephyrok@gmail.com>
2021-11-17 22:41:02 -0600

    Added project java/micronaut/petstore for codegen from api spec

---
+++ java/micronaut/petstore/src/main/java/com/zephyrok/Application.java
@@ -0,0 +1,10 @@
+package com.zephyrok;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Micronaut.run(Application.class, args);
+    }
+}
